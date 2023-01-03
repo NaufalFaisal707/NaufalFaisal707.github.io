274786de22176f2360875f39ddb1fcc377ab7d37
deffelovers <deffelovers.code@gmail.com>
2023-01-03 11:55:05 +0000

    push ajah

--- src/js/page.js
+++ src/js/page.js
@@ -8,20 +8,20 @@ $(() => {
 
     // navbar indicator
     const navItems = [
-        {'name':'beranda', 'icon':'fa-house'},
-        {'name':'Pendidikan', 'icon':'fa-graduation-cap'},
-        {'name':'Kemampuan & Keahlian', 'icon':'fa-wand-magic-sparkles'},
-        {'name':'Proyek Saya', 'icon':'fa-diagram-project'},
-        {'name':'Sertifikat', 'icon':'fa-award'},
-        {'name':'Kontak Saya', 'icon':'fa-headset'},
+        {'name':'beranda', 'icon':'<i class="fa-solid fa-house"></i>'},
+        {'name':'Pendidikan', 'icon':'<i class="fa-solid fa-graduation-cap"></i>'},
+        {'name':'Kemampuan & Keahlian', 'icon':'<i class="fa-solid fa-wand-magic-sparkles"></i>'},
+        {'name':'Proyek Saya', 'icon':'<i class="fa-solid fa-diagram-project"></i>'},
+        {'name':'Sertifikat', 'icon':'<i class="fa-solid fa-award"></i>'},
+        {'name':'Kontak Saya', 'icon':'<i class="fa-solid fa-headset"></i>'},
     ]
     
     // Body structure
     const body = $('body')
     body.attr({
         'data-bs-spy': 'scroll',
-        'data-bs-target': '#mynav'
-    })
+        'data-bs-target': '#mynav',
+    }).addClass('user-select-none')
     // navbar
     body.html(`
     <nav id="mynav" class="navbar fixed-top navbar-expand-lg navbar-dark">
@@ -32,14 +32,20 @@ $(() => {
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto"></ul>
+                <ul class="navbar-nav ms-auto">
+                    ${navItems.map((val, inx) => {
+                        return `
+                            <li class="nav-item">
+                                <a class="nav-link" href="#page${inx}">
+                                    ${val['icon']} ${val['name']}
+                                </a>
+                            </li>`
+                    }).join('')}
+                </ul>
             </div>
         </div>
     </nav>
     `)
-    $.each(navItems, function(inx, val){
-        $('nav ul.navbar-nav').append(`<li class="nav-item"><a class="nav-link" href="#page${inx}"><i class="fa-solid ${val['icon']}"></i> ${val['name']}</a></li>`)
-    })
     
     // body
     $.each(navItems, function(inx, val){
@@ -47,30 +53,52 @@ $(() => {
         inx === 0
             ? $(`section#page${inx}`).addClass('container')
             .html(`
-                <div class="vh-100 d-flex flex-lg-row flex-column-reverse justify-content-center align-items-center">
-                    <span class="text-center text-lg-start">
-                        <p class="h1">
+            <div class="vh-100 d-flex flex-column-reverse flex-lg-row justify-content-center align-items-center">
+                <div class="text-center text-lg-start">
+                    <p class="h1">
                         ${web.ownerBio[0].split('[nama]').join(web.ownerName)}
-                        </p>
-                        <p class="fs-6">
-                            ${web.ownerBio[1].split('[umur]').join(web.ownerAge())}
-                        </p>
-                    </span>
-                    <img class="col-10 col-sm-6 col-md-6 col-lg-6" src="${web.webBrandPic}" width="auto" height="auto" alt="" srcset="">
+                    </p>
+                    <p class="fs-6">
+                        ${web.ownerBio[1].split('[umur]').join(web.ownerAge())}
+                    </p>
+                    <div class="fs-4 d-flex justify-content-center justify-content-lg-start gap-5 pt-2">
+                        ${
+                            web.ownerSocial.map(val => {
+                                return `
+                                    <a href="${val.url}" aria-label="link media sosial" target="_blank" class="url drac-text-white">${val.icon}</a>
+                                    `
+                            }).join('')
+                        }
+                    </div>                
                 </div>
+                <img class="col-10 col-sm-6 col-md-6 col-lg-6" src="${web.webBrandPic}" width="auto" height="auto" alt="" srcset="">
+            </div>
             `)
                 
         : $(`section#page${inx}`).addClass('container')
-        // <div class="py-4">
-        //     <i class="fa-solid ${val['icon']}"></i> ${val['name']}
-        // </div>
         .html(`
-        <div class="vh-100 d-flex flex-column justify-content-center align-items-center">
-            <div class="">
-                judul
+        <div class="min-vh-100 d-flex flex-column justify-content-md-evenly align-items-center auto-pad">
+            <div class="h4 py-4 py-md-0">
+                ${val['icon']} ${val['name']}
             </div>
-            <div class="">
-                konten
+            <div class="d-flex justify-content-center flex-wrap gap-5">
+            ${
+                inx === 1
+                ? web.person.education.map(val => {
+                    return `
+                    <div variant="subtle" class="drac-box drac-card drac-card-subtle drac-border-white drac-bg-white drac-p-md">
+                        <div class="card-img-top school-image rounded" style="${val.picURL}"></div>
+                        <div class="card-body pt-4">
+                            <h4 class="card-title drac-text-pink">${val.year}</h4>
+                            <p class="card-text fs-5">${val.school}</p>
+                        </div>
+                    </div>
+                    `
+                }).join('')
+            : inx === 1
+                ? `<div>jancok pristel</div>`
+            : ``
+            }
             </div>
         </div>
         `)
@@ -79,11 +107,24 @@ $(() => {
     // page script
     $(window).scroll(function(){
         (window.pageYOffset >= 100)
-            ? $('nav').addClass('dra-secondary shadow').css('transition', '.2s')
-        : $('nav').removeClass('dra-secondary shadow')
+            ? $('nav').addClass('shadow').css({
+                'transition': '.2s',
+                'background-color': 'var(--blackSecondary)'
+            })
+        : $('nav').removeClass('shadow').css('background-color', 'transparent')
     })
-    
-    
+
+                    // `
+                    //     <div class="card bg-transparent border-start border-5 text-primary" style="width: 20rem">
+                            // <div class="card-img-top school-image"></div>
+                            // <div class="card-body">
+                            //     <h4 class="card-title txt-pink">${val.year}</h4>
+                            //     <p class="card-text fs-5">${val.school}</p>
+                            //     <button class="drac-btn drac-bg-purple drac-btn-outline drac-text-purple">Outline</button>
+                            // </div>
+                    //     </div>
+                    // `
+
     // inx === 0
     //     ? (function(){
     //         $(`section#${inx}`).append('<div class="container"/>').ready(() => {
--- src/js/profile.js
+++ src/js/profile.js
@@ -2,20 +2,41 @@ module.exports = {
     webBrandName: 'Naufal.f',
     webBrandUrl: '#',
     webBrandPic: require('../asset/img/Page.png'),
-    ownerName: '<span class="txt-danger">Naufal</span> Faisal',
+    ownerName: '<span class="drac-text-red">Naufal</span> Faisal',
     ownerBio: [`Hai!, Saya [nama]`, 'sekarang saya berumur [umur] tahun, dan saya senang mempelajari sesuatu yang berhubugnan dengan perkembangan teknologi, programing dan editing.'],
     ownerBirthYear: 2005,
-    ownerSocial: {
-        facebook:   '',
-        instagram:  '',
-        twitter:    '',
-        git:        ''
-    },
+    ownerSocial: [
+        {icon: '<i class="bi bi-facebook"></i>', url: '#'},
+        {icon: '<i class="bi bi-instagram"></i>', url: '#'},
+        {icon: '<i class="bi bi-twitter"></i>', url: '#'},
+        {icon: '<i class="bi bi-github"></i>', url: '#'}
+    ],
     ownerContact: {
         email: '',
-        telegram:   '',
+        telegram: '',
     },
-    ownerHobbies: 'satir 👊😎',
+    person: {
+        education: [
+            {
+                year: '2010 - 2016',
+                school: 'SDS Binamuslimin',
+                picURL: `background-image: url('${require('../asset/img/pic0.jpg')}')`
+            },
+            {
+                year: '2017 - 2019',
+                school: 'SMP KP Ciparay',
+                picURL: `background-image: url('${require('../asset/img/pic2.jpg')}')`
+            },
+            {
+                year: '2020 - 2023',
+                school: 'SMK 2 LPPM-RI Majalaya',
+                picURL: `background-image: url('${require('../asset/img/pic1.jpg')}')`
+            },
+        ],
+        skill: {
+            
+        }
+    },
 
 
     // function
