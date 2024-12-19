60f0fe44a13093501e4a3e66b97d21f0637dd9e3
NaufalFaisal707 <naufalfaisal707@gmail.com>
2024-12-19 09:58:47 +0700

    update skill

--- app/routes/_page._index.tsx
+++ app/routes/_page._index.tsx
@@ -1,3 +1,13 @@
 export default function IndexPage() {
-  return <h1>alo ini index!</h1>;
+  return (
+    <div className="p-4 m-4">
+      <h1 className="underline font-semibold">may skil:</h1>
+      <ol className="list-disc">
+        <li>html</li>
+        <li>css</li>
+        <li>javascript</li>
+        <li>jquery</li>
+      </ol>
+    </div>
+  );
 }
--- app/routes/_page.tsx
+++ app/routes/_page.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "@remix-run/react";
+import Container from "~/components/container";
 
 export default function Page() {
   return (
-    <>
-      <p>alo ini root nya!</p>
+    <Container>
+      <h1 className="text-xl p-4 font-bold">
+        selamat datang di web port folio saya.
+      </h1>
       <Outlet />
-    </>
+    </Container>
   );
 }
